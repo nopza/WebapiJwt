6cc61f9e9a9862871054996120d874cf6e83048b
nopza <44990151+nopza@users.noreply.github.com>
2021-03-01 00:20:40 +0700

    create method get List of user

---
+++ WebapiJwt/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebapiJwt.Data;
+using WebapiJwt.Models;
+
+namespace WebapiJwt.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/account")]
+        public IActionResult Get()
+        {
+            IEnumerable<User2> objList = _db.User2;
+            var queryUser = objList.Select(x => new
+            {
+                x.Username,
+                x.Name,
+                x.Age
+            }).ToList();
+            return Json(queryUser);
+        }
+    }
+}
